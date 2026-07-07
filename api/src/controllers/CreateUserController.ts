4383f889315e14103dc6227e1e27bff8f4622925
Gabriel <trevisani@alunos.utfpr.edu.br>
2026-07-07 09:42:57 -0300

    feat: usuarios novos bloqueados

--- api/src/controllers/CreateUserController.ts
+++ api/src/controllers/CreateUserController.ts
@@ -2,19 +2,25 @@ import type { Request, Response } from "express";
 import { CreateUserService } from "../services/CreateUserService.js";
 
 class CreateUserController {
-    static async handle(req: Request, res: Response) {
-        const { name, email, password } = req.body;
+  static async handle(req: Request, res: Response) {
+    //temporariamente desabilitado
+    return res.status(403).json({
+      postMessage:
+        "o cadastro de novos usuários está temporariamente desabilitado",
+    });
 
-        const createUserService = new CreateUserService();
+    const { name, email, password } = req.body;
 
-        const user = await createUserService.execute({
-            name,
-            email,
-            password
-        });
+    const createUserService = new CreateUserService();
 
-        return res.json(user);
-    }
+    const user = await createUserService.execute({
+      name,
+      email,
+      password,
+    });
+
+    return res.json(user);
+  }
 }
 
 export { CreateUserController };
