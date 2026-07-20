cde8129901572a89c80b87ecb80ef7999ba2e849
Gabriel <trevisani@alunos.utfpr.edu.br>
2026-07-20 14:17:43 -0300

    fix: todo rolo com visit e notification

--- api/src/controllers/CreateUserController.ts
+++ api/src/controllers/CreateUserController.ts
@@ -4,10 +4,6 @@ import { CreateUserService } from "../services/CreateUserService.js";
 class CreateUserController {
   static async handle(req: Request, res: Response) {
     //temporariamente desabilitado
-    return res.status(403).json({
-      postMessage:
-        "o cadastro de novos usuários está temporariamente desabilitado",
-    });
 
     const { name, email, password } = req.body;
 
